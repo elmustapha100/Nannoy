434bcf44b90ce2bcdc8607da2743e5b77604ab03
EL Mustapha <elmustaphaabimbola628@gmail.com>
2026-06-02 01:13:46 +0100

    Workshop Number 5 on functions

--- nannoy_be/freecodecamp_workshop/workshop_5.py
+++ nannoy_be/freecodecamp_workshop/workshop_5.py
@@ -36,3 +36,22 @@
 # Waiting:11. apply_discount(74.5, 20.0) should return 59.6.
 
 
+def apply_discount(price , discount): 
+    if not isinstance(price ,(int,float)):
+        return "The price should be a number."
+    
+    if not isinstance(discount,(int,float)): 
+        return "The discount should be a number."
+
+    if price <= 0 : 
+        return "The price should be greater than 0."
+
+    if discount < 0 or discount > 100 : 
+        return "The discount should be between 0 and 100."    
+
+    amount = price * (discount/100)    
+    return price - amount
+print(apply_discount(100,20))    
+print(apply_discount(200,50))   
+print(apply_discount(50,0))
+print(apply_discount(74.5,20.0))      
