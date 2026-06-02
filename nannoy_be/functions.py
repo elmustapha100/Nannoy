327f30a771d625c71359b72394fe517d678884ea
EL Mustapha <elmustaphaabimbola628@gmail.com>
2026-06-02 02:36:03 +0100

    omor!

--- nannoy_be/functions.py
+++ nannoy_be/functions.py
@@ -1,7 +1,11 @@
-def greet(name):
+# def greet(name):
 
-    message = "Hello " + name + "!"
-    return message 
+#     message = "Hello " + name + "!"
+#     return message 
 
-result = greet("Mustapha")
-print(result)    
+# result = greet("Mustapha")
+# print(result)    
+
+def function():
+    base_url = 'https://api.example.com'
+    
