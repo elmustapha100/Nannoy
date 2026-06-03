4b5ba6cffc4e3f94e39304e19a481c1cf299fed4
EL Mustapha <elmustaphaabimbola628@gmail.com>
2026-06-03 23:09:12 +0100

    Done

--- nannoy_be/freecodecamp_workshop/caesar_cipher.py
+++ nannoy_be/freecodecamp_workshop/caesar_cipher.py
@@ -1,20 +1,35 @@
-def caesar(text , shift, mode): 
-    if not ininstance(text , shift ,(str,int)):
+def caesar(text , shift, encrypt=True): 
+    if not isinstance(text , str) or not isinstance(shift,int):
         return "error"
     if shift < 1 or shift > 25 : 
         return "Error, shift must be an integer between 1-25" 
-        
+
+    if not encrypt :
+        shift = -shift     
+
+    encrypted_text = ""    
+
     for i in text : 
         if i.isalpha(): 
             if i.isupper():
-                ascii_value = ord(i)   
-            else :
-                ascii_value = ord(i)    
+              base = ord('A')
+            else :  
+                base = ord('a')      
             #formula such that it convert to 0–25 range
 
-            position = ord(i) - ascii_value
+            position = ord(i) - base
             shifted_text = (position + shift)%26   #if z = 26 and shift = 6 , the output should be f .
-            encrypted_text += chr(shifted_text + position) 
-            return enrypted_text 
-        else :
-            pass()    
+            encrypted_text += chr(shifted_text + base) 
+        else : 
+            encrypted_text += i     
+    return encrypted_text
+
+def encrypt(text,shift):
+    return caesar(text, shift)
+
+def decrypt(text,shift) :      
+    return caesar(text,shift,encrypt =False)
+
+encrypted_text = input("Enter your message : ")
+decrypted_text = decrypt(encrypted_text, 13)
+print(decrypted_text)
