774ba295fdde9c14d5130e555c45a1300f3b745d
EL Mustapha <elmustaphaabimbola628@gmail.com>
2026-07-23 18:59:56 +0100

    All test passed

--- nannoy_be/freecodecamp_workshop/player_interface.py
+++ nannoy_be/freecodecamp_workshop/player_interface.py
@@ -54,3 +54,33 @@
 # Waiting:23. The Pawn's __init__ method should set the moves attribute to a list of tuples representing x, y coordinates, where each coordinate tuple represents a movement of 1 unit in the following directions: up, down, left, right.
 # Waiting:24. The Pawn class should have a level_up method.
 # Waiting:25. The Pawn's level_up method should add the four diagonal movement of 1 unit to the moves attribute.
+
+
+from abc import ABC , abstractmethod 
+import random
+class Player(ABC):
+    def __init__(self):
+        self.moves = []
+        self.position = (0,0)
+        self.path = [self.position]
+
+    def make_move(self):
+        move = random.choice(self.moves)
+        new_x = self.position[0] + move[0]
+        new_y = self.position[1] + move[1]
+        self.position = (new_x, new_y)
+        self.path.append(self.position)
+        return self.position
+
+    @abstractmethod
+    def level_up(self):
+        pass
+
+class Pawn(Player):
+    def __init__(self):
+        super().__init__()
+        self.moves = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+        
+    def level_up(self):
+        self.moves.extend([(1, 1), (1, -1), (-1, 1), (-1, -1)])
+
