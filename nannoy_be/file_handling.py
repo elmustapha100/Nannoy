62968e2ab300d9e4adac288b27abce2a3763a32e
EL Mustapha <elmustaphaabimbola628@gmail.com>
2026-07-28 22:00:18 +0100

    Implement file handling (writing to a file,overwiting toa file & create  a ne file) and create new file

--- nannoy_be/file_handling.py
+++ nannoy_be/file_handling.py
@@ -15,8 +15,27 @@ print(f.readline())
 print(f.readline())
 print(f.readline())
 
-Loop through the file line by line:
+#Loop through the file line by line:
 
 with open("inheritance.py",'r') as f :
    for i in f :
         print(i)
+
+# writing to a file
+
+with open("text.txt",'a') as t :
+    t.write("In history of the biggest prize in Mathematics , there are only 3 female mathematicians that got the fields medal.")
+
+# #opening the file after appending 
+ with open("text.txt",'r') as t :
+    print(t.read())
+
+#overwriting the content of file
+with open('text.txt','w') as t :
+    t.write("Oooops! File content deleted!")
+
+# # with open("text.txt") as t :
+# #     print(t.read())  
+
+#creating a new file
+ new_file = open("Newfile.txt","x")          
