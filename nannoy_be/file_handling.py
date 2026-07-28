5d816e59cc6384010a71e5e9433417c65232c451
EL Mustapha <elmustaphaabimbola628@gmail.com>
2026-07-28 23:32:05 +0100

    Refactor file handling examples for clarity and consistency

--- nannoy_be/file_handling.py
+++ nannoy_be/file_handling.py
@@ -1,41 +1,50 @@
-#In python, opening a file goes like this 
+# #In python, opening a file goes like this 
+# import os
+# f = open("variable.py","r")
+# print(f.read()) #it opens and reads the entire file 
 
-f = open("variable.py","r")
-print(f.read()) #it opens and reads the entire file 
+# #opening a file using the with statement 
 
-#opening a file using the with statement 
-
-with open("abstraction_oop.py","r") as f :
-    print(f.read())
-    f.close() #closing a file
+# with open("abstraction_oop.py","r") as f :
+#     print(f.read())
+#     f.close() #closing a file
 
 
-#Read Line method , it returns the first line of the file 
-print(f.readline())
-print(f.readline())
-print(f.readline())
+# #Read Line method , it returns the first line of the file 
+# print(f.readline())
+# print(f.readline())
+# print(f.readline())
 
-#Loop through the file line by line:
+# #Loop through the file line by line:
 
-with open("inheritance.py",'r') as f :
-   for i in f :
-        print(i)
+# with open("inheritance.py",'r') as f :
+#    for i in f :
+#         print(i)
 
-# writing to a file
+# # writing to a file
 
-with open("text.txt",'a') as t :
-    t.write("In history of the biggest prize in Mathematics , there are only 3 female mathematicians that got the fields medal.")
+# with open("text.txt",'a') as t :
+#     t.write("In history of the biggest prize in Mathematics , there are only 3 female mathematicians that got the fields medal.")
 
-# #opening the file after appending 
- with open("text.txt",'r') as t :
-    print(t.read())
+# # #opening the file after appending 
+# with open("text.txt",'r') as t :
+#     print(t.read())
 
-#overwriting the content of file
-with open('text.txt','w') as t :
-    t.write("Oooops! File content deleted!")
+# #overwriting the content of file
+# with open('text.txt','w') as t :
+#     t.write("Oooops! File content deleted!")
 
-# # with open("text.txt") as t :
-# #     print(t.read())  
+# # # with open("text.txt") as t :
+# # #     print(t.read())  
 
 #creating a new file
- new_file = open("Newfile.txt","x")          
+new_file = open("Newfile.txt","x")     
+
+#deleting a file:import os   
+os.remove("Newfile.txt")    
+
+if os.path.exist("Newfile.txt"):
+    os.remove("Newfile.txt")
+
+else :
+    print("file doesn't exist")    
