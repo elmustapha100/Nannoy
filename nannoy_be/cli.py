8e3f8be42dde87847f8977dd369873290b2a47fa
EL Mustapha <elmustaphaabimbola628@gmail.com>
2026-08-04 18:55:34 +0100

    Add initial CLI script to handle command line arguments

---
+++ nannoy_be/cli.py
@@ -0,0 +1,6 @@
+import sys 
+#This module allow access to variables used/maintained by the intepreter , include to run with the runtime env.
+#& sys.argv is one variable used to handle command line argument 
+
+print(sys.argv[0])  #output :the name of the script itself 
+print(str(sys.argv))
